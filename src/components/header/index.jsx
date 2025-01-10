4a2613db54b23202dadb08c05e9ca177d4946a1c
abazdev34 <abaztoktorbaev89@gmail.com>
2025-01-10 22:49:42 +0300

    sfsfgsgdgdfg

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -18,38 +18,7 @@ const Header = () => {
         </button>
         <nav className={`menu ${isMenuOpen ? 'open' : ''}`}>
           <ul>
-            <li><Link to="/">Главная</Link></li>
-            <li><Link to="/texKarta">Техкарта</Link></li>
-            <li><Link to="/ovoshi">Овоши</Link></li>
-            <li><Link to="/timer">фасольвойпаста</Link></li>
-          </ul>
-        </nav>
-      </div>
-    </header>
-  );
-};
-
-export default Header;import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import './header.scss';
-
-const Header = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
-
-  return (
-    <header className="app-header">
-      <div className="header-content">
-        <h1>Инвентаризация</h1>
-        <button className="menu-toggle" onClick={toggleMenu}>
-          ☰
-        </button>
-        <nav className={`menu ${isMenuOpen ? 'open' : ''}`}>
-          <ul>
-            <li><Link to="/">Главная</Link></li>
+     
             <li><Link to="/texKarta">Техкарта</Link></li>
             <li><Link to="/ovoshi">Овоши</Link></li>
             <li><Link to="/timer">фасольвойпаста</Link></li>
