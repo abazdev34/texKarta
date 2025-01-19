0de0ad6839830d7d1ee664f648a9eba5da80910a
abazdev34 <abaztoktorbaev89@gmail.com>
2025-01-20 00:48:51 +0300

    ADADAF

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,28 +1,30 @@
-
 import { Link } from 'react-router-dom';
+import { IconButton } from '@mui/material';
+import TimerIcon from '@mui/icons-material/Timer';
 import './header.scss';
 
 const Header = () => {
+  return (
+    <div className="header">
 
   
-
-  return (
-
-     
-       <div className="header">
-         <Link to="/texKarta">Техкарта</Link>
-            <Link to="/ovoshi">Овоши</Link>
-            <Link to="/timer">фасольвойпаста</Link>
-
-       </div>
+        <IconButton component={Link} to="/texKarta">
+ 
+        Техкарта 
+      </IconButton>
       
-   
-          
      
-           
-          
-
       
+      <IconButton component={Link} to="/ovoshi">
+        <TimerIcon />
+       Овощей 
+      </IconButton>
+      
+      <IconButton component={Link} to="/timer">
+        <TimerIcon />
+        фасолевойпаста
+      </IconButton>
+    </div>
   );
 };
 
