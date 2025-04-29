0116eaf364a77e9ebb2bc0e95327a868105690de
abazdev34 <abaztoktorbaev89@gmail.com>
2025-04-29 05:09:43 +0300

    cgdzzgdzdfzzx

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Header from './components/header'
 import Timer_ovoshi from './components/ovoshi/index.jsx'
 import Timer from './components/fPasta/fTaimer.jsx'
 import SousTimer from './components/sous/idex.jsx'
+import Texmyso from './components/texKarta/texMyso.jsx'
 
 function App() {
   return (
@@ -18,6 +19,8 @@ function App() {
     <Route path="/ovoshi" element={<Timer_ovoshi/>} />
     <Route path="/timer" element={<Timer/>} />
     <Route path="/Sous" element={<SousTimer />} />
+    <Route path="/Texmyso" element={<Texmyso />} />
+    
    
     
 
