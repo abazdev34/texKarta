5091a1530b6c2ad452308b1991a7df0e0efeb498
abazdev34 <abaztoktorbaev89@gmail.com>
2025-04-29 06:43:55 +0300

    вававппыапывав

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,36 +1,33 @@
-
 import { Link } from 'react-router-dom';
 import './header.scss';
 import TimerIcon from '@mui/icons-material/Timer';
-import { GiChickenOven } from "react-icons/gi";
+import { GiChickenOven, GiTacos } from "react-icons/gi";
+import { useEffect, useState } from 'react';
 
-import { GiTacos } from "react-icons/gi";
 const Header = () => {
+  const [isLaptop, setIsLaptop] = useState(window.innerWidth >= 1024); // Ноутбук өлшемі
 
-  
+  useEffect(() => {
+    const handleResize = () => {
+      setIsLaptop(window.innerWidth >= 1024);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   return (
-
-     
-       <div className="header">
-         <Link to="/texKarta"> <GiTacos />   Техкарта</Link>
-         
-            <Link to="/ovoshi">    <TimerIcon />Овощи жареные</Link>
-            <Link to="/timer">    <TimerIcon />Фасолеваяпаста</Link>
-            <Link to="/Sous">    <TimerIcon />Соус</Link>
-            <Link to="/Texmyso">   <GiChickenOven />Готовый продук</Link>
-         
-
-
-       </div>
-      
-   
-          
-     
-           
-          
-
-      
+    <div className="header">
+      <Link to="/texKarta"><GiTacos /> Техкарта</Link>
+      <Link to="/ovoshi"><TimerIcon /> Овощи жареные</Link>
+      <Link to="/timer"><TimerIcon /> Фасолевая паста</Link>
+      <Link to="/Sous"><TimerIcon /> Соус</Link>
+      {isLaptop && (
+        <Link to="/Texmyso"><GiChickenOven /> Готовый продук</Link>
+      )}
+    </div>
   );
 };
 
